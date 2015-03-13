05ec952632eb3f37613b8d7d0869a47d2c4758b8
RockstomProgramming <lespaulg1@gmail.com>
2015-03-13 08:23:38 -0300

    Otimização das telas

---
+++ src/main/java/br/com/finan/annotation/Unique.java
@@ -0,0 +1,23 @@
+package br.com.finan.annotation;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Arquivo: Unique.java <br/>
+ *
+ * @author Wesley Luiz
+ * @version 2.0
+ */
+@Target({ METHOD, FIELD })
+@Retention(RUNTIME)
+@Documented
+public @interface Unique {
+
+	String message() default "";
+}
